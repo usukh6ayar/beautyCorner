ed634a31718cfd99d2769ed4fc6b78a79f8a84a9
usukhbayar.gant <usukhbayar.gant@nomin.net>
2025-05-02 03:07:04 +0800

    update

--- App.js
+++ App.js
@@ -3,14 +3,19 @@ import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/navigation/AppNavigator";
 import { Provider as PaperProvider } from "react-native-paper";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <StatusBar barStyle="dark-content" />
-        <AppNavigator />
-      </NavigationContainer>
-    </PaperProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AppNavigator />
+    </GestureHandlerRootView>
   );
+  // Өмнөхөн кодыг ашиглахад:
+  //   <PaperProvider>
+  //   <NavigationContainer>
+  //     <StatusBar barStyle="dark-content" />
+  //     <AppNavigator />
+  //   </NavigationContainer>
+  // </PaperProvider>
 }
